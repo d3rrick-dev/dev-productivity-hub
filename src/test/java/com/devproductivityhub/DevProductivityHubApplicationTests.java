c991e499320bde23c403370eb7714a4ac4084bbe
derrick <muriithiderrick93@gmail.com>
2025-11-27 12:35:47 +0300

    Fix version

--- src/test/java/com/devproductivityhub/DevProductivityHubApplicationTests.java
+++ src/test/java/com/devproductivityhub/DevProductivityHubApplicationTests.java
@@ -3,11 +3,18 @@ package com.devproductivityhub;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class DevProductivityHubApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void sampleTest() {
+        assertThat(2 + 2).isEqualTo(3);
     }
 
 }
