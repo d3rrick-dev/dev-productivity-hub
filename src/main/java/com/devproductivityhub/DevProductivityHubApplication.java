5e1b3b31385e519c8ccf5cd7b8f3fef82f0c8bfa
derrick <muriithiderrick93@gmail.com>
2025-11-27 12:23:46 +0300

    Add PR feedback

---
+++ src/main/java/com/devproductivityhub/DevProductivityHubApplication.java
@@ -0,0 +1,13 @@
+package com.devproductivityhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevProductivityHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevProductivityHubApplication.class, args);
+    }
+
+}
